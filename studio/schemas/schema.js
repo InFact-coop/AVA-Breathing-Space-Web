d1e44ce588d11c58c5cb4bb2c179ec02081a24f5
lucymk <lucy.m.king92@gmail.com>
2020-05-21 16:20:57 +0100

    prettier web

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -5,8 +5,6 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // Then we give our schema to the builder and provide the result to Sanity
-console.log('hello')
-
 export default createSchema({
   // We name our schema
   name: 'default',
