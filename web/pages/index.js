b4f10c2a3dd98a2a66e9da81b7ec6a0d078f5a20
Max Gerber <mgerber94@gmail.com>
2020-05-21 18:09:38 +0100

    + Added Apollo to project  + Interfacing with Sanity
     + Sanity frontend working in canary, backend working on phone...

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,7 +1,23 @@
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const GET_SERVICES_QUERY = gql`
+  query GET_SERVICES_QUERY {
+    allService {
+      name
+    }
+  }
+`;
 const Index = () => {
   return (
     <div>
       <p>Hello world!</p>
+      <Query query={GET_SERVICES_QUERY}>
+        {(payload) => {
+          console.log("we out here", payload);
+          return <p>{JSON.stringify(payload.data)}</p>;
+        }}
+      </Query>
     </div>
   );
 };
