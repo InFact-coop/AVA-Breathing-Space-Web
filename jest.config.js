28110c8a6d9fbd53218cbb0be9b6b5cd05edc91a
lucymk <lucy.m.king92@gmail.com>
2020-05-21 16:13:00 +0100

    move out of packages folder and point to root test folder

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  ...require('./test/jest-common'),
+  ...require('./web/test/jest-common'),
   projects: ['./test/jest.lint.js'],
 }
--- packages/web/jest.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  ...require('./test/jest-common'),
-  projects: ['./test/jest.lint.js'],
-}
