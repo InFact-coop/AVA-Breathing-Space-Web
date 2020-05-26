e79d697a31d78485f42703a524d1321552f982bc
Max Gerber <mgerber94@gmail.com>
2020-05-26 11:21:31 +0100

    - Working GraphQL "get" request to Sanity

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,5 +1,6 @@
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import * as R from "ramda";
 
 const GET_SERVICES_QUERY = gql`
   query GET_SERVICES_QUERY {
@@ -8,14 +9,16 @@ const GET_SERVICES_QUERY = gql`
     }
   }
 `;
+
+const Service = ({ name }) => <p>{name}</p>;
 const Index = () => {
   return (
     <div>
-      <p>Hello world!</p>
       <Query query={GET_SERVICES_QUERY}>
-        {(payload) => {
-          console.log("we out here", payload);
-          return <p>{JSON.stringify(payload.data)}</p>;
+        {({ data, error, loading }) => {
+          if (loading) return <p>LOADING...</p>;
+          if (error) return <p>Error: {error.message}</p>;
+          return R.map(Service)(data.allService);
         }}
       </Query>
     </div>
