13443114457e5a339697abe2a72fc39f4e4f863b
Max Gerber <mgerber94@gmail.com>
2020-05-27 10:48:19 +0100

    Merged staging

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,9 +1,17 @@
+import '../styles/index.css'
+
 import App from 'next/app'
 import { ApolloProvider } from 'react-apollo'
+import resolveConfig from 'tailwindcss/resolveConfig'
+import { ThemeProvider } from 'styled-components'
 import withData from '../lib/withData'
 
 import Page from '../components/Page'
 import Meta from '../components/Meta'
+
+import tailwindConfig from '../tailwind.config.js' //eslint-disable-line
+
+const { theme } = resolveConfig(tailwindConfig)
 
 class BreathingSpace extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -20,10 +28,12 @@ class BreathingSpace extends App {
     const { Component, apollo, pageProps } = this.props
     return (
       <ApolloProvider client={apollo}>
-        <Meta />
-        <Page>
-          <Component {...pageProps} />
-        </Page>
+        <ThemeProvider theme={theme}>
+          <Meta />
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ThemeProvider>
       </ApolloProvider>
     )
   }
--- web/pages/index.js
+++ web/pages/index.js
@@ -1,6 +1,7 @@
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 import * as R from 'ramda'
+import styled from 'styled-components'
 
 const GET_SERVICES_QUERY = gql`
   query GET_SERVICES_QUERY {
@@ -10,14 +11,19 @@ const GET_SERVICES_QUERY = gql`
   }
 `
 
-const Service = ({ name }) => <p>{name}</p>
+const ServiceStyled = styled.p.attrs({
+  className: 'bg-coral font-header martin_sheen',
+})``
+
+const Service = ({ name }) => <ServiceStyled>{name}</ServiceStyled>
 const Index = () => {
   return (
     <div>
       <Query query={GET_SERVICES_QUERY}>
         {({ data, error, loading }) => {
           if (loading) return <p>LOADING...</p>
-          if (error) return <p>Error: {error.message}</p>
+          if (error)
+            return <ServiceStyled>Error: {error.message}</ServiceStyled>
           return R.map(Service)(data.allService)
         }}
       </Query>
