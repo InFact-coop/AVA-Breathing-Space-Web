b73c2943f8bd16dd96e0a17ff5ed1133c1223d30
AVA-BreathingSpace <ava-breathingspace@users.noreply.github.com>
2020-06-02 15:01:13 +0100

    gets technique from Sanity

--- web/pages/self-care/[technique].js
+++ web/pages/self-care/[technique].js
@@ -1,11 +1,31 @@
 import styled from 'styled-components'
+import client from '../../client'
+import { useState } from 'react'
+
+const GET_SELF_CARE_TECHNIQUE = `*[_type == "selfcareTechnique" && slug.current == $slug][0]{
+  title,
+  mainImage,
+  body, 
+  video, 
+  audio, 
+  link, 
+  likes
+}`
 
 const TechniqueStyled = styled.section.attrs({
   className: '',
 })``
 
-const Technique = () => {
+const Technique = props => {
+  const [technique, setTechnique] = useState(props)
+
   return <TechniqueStyled>Technique</TechniqueStyled>
+}
+
+Technique.getInitialProps = ctx => {
+  return client.fetch(GET_SELF_CARE_TECHNIQUE, {
+    slug: ctx.query.technique,
+  })
 }
 
 export default Technique
