8db39ac41532e56cd0b7ab10e3b407c4a046dc87
AVA-BreathingSpace <ava-breathingspace@users.noreply.github.com>
2020-06-02 12:08:17 +0100

    changes for pushing to heroku

--- public/sw.js
+++ public/sw.js
@@ -109,7 +109,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "2fdd189ea17faa56a4f44a2fdd88b84f"
   }, {
     "url": "/_next/static/runtime/main.js",
-    "revision": "5630508b3f538a445b002facbed188d9"
+    "revision": "bb855f8c73e9aaeeb01eb55fe2dd23ed"
   }, {
     "url": "/_next/static/runtime/polyfills.js",
     "revision": "516c3d4deeff70783126396a5a14d34e"
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "02492051c17306f7efe9c811dd2c49a6"
+    "revision": "d11d621a06c2dd39aecc61db8187397e"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
