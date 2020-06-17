6ed811f5dbfc446bd4e171d2173084b56466611c
lucymk <lucy.m.king92@gmail.com>
2020-06-17 17:04:24 +0100

    update env variables to create documents using client

--- client.js
+++ client.js
@@ -1,12 +1,11 @@
 const sanityClient = require('@sanity/client')
 
-
-
 const client = sanityClient({
   projectId: 'is8j72h6',
   dataset: process.env.SANITY_DATASET,
-  token: '', // or leave blank to be anonymous user
-  useCdn: true, // `false` if you want to ensure fresh data
+  token: process.env.SANITY_TOKEN, // or leave blank to be anonymous user
+  useCdn: false, // `false` if you want to ensure fresh data
+  withCredentials: true,
 })
 
 module.exports = client
