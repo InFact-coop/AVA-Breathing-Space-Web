9b5a5648c3ece6e9227eb3cf10ffbc33cfbdb54b
lucymk <lucy.m.king92@gmail.com>
2020-06-17 17:47:26 +0100

    abstract project ID into env v

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const sanityClient = require('@sanity/client')
 
 const client = sanityClient({
-  projectId: 'is8j72h6',
+  projectId: process.env.SANITY_ID,
   dataset: process.env.SANITY_DATASET,
   token: process.env.SANITY_TOKEN, // or leave blank to be anonymous user
   useCdn: false, // `false` if you want to ensure fresh data
