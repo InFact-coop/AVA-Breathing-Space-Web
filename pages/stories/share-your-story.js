8a00da7b0b77dabd3a0f2efccfc4504209fe17c6
lucymk <lucy.m.king92@gmail.com>
2020-06-17 17:06:48 +0100

    move form components to own page and add form functionality

--- pages/stories/share-your-story.js
+++ pages/stories/share-your-story.js
@@ -1,19 +1,25 @@
 import styled from 'styled-components'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import * as R from 'ramda'
+import { v4 as uuidv4 } from 'uuid'
 
 import client from '../../client'
+
+import useForm from '../../lib/useForm'
+import toCamelCase from '../../lib/toCamelCase'
 
 import Container from '../../components/Container'
 import { PurpleButton, CoralButton } from '../../components/Button'
 import Block from '../../components/Block'
+import { FormFieldWithLabel } from '../../components/Form'
 
 const GET_SHARE_STORY_FORM = `*[_type == "form" && slug.current == "share-your-story"][0]{
   _type, 
   title, 
   body, 
   subtitle, 
-  "inputs": inputs[]->{ title, required, type },
+  "inputsFromSanity": inputs[]->{ title, required, type },
+  confirmationText
 }`
 
 const ShareStoryStyled = styled(Container).attrs({
@@ -24,41 +30,51 @@ const Title = styled.h2.attrs({
   className: 'font-serif text-lg leading-base mb-2.5',
 })``
 
-const StyledInput = styled.input.attrs({
-  className: 'border border-lightgray h-8.75 p-2.5 rounded-2.5',
-})``
-
-const StyledTextArea = styled.textarea.attrs({
-  className: 'border border-lightgray rounded-2.5 p-2.5',
-  rows: '9',
-})``
-
-const Label = styled.label.attrs({
-  className: 'mb-1.25',
-})``
-
-const FormFieldWithLabel = ({ title, required, type }) => {
-  const Field = () => {
-    switch (type) {
-      case 'input':
-        return <StyledInput required={required} />
-      case 'textarea':
-        return <StyledTextArea required={required} />
-      default:
-        return <div />
+const ShareStory = ({ body, inputsFromSanity, subtitle, confirmationText }) => {
+  const uploadStoryToSanity = (inputs, setInputs, initialState) => {
+    const story = {
+      _id: `drafts.${uuidv4()}`,
+      _type: 'story',
+      author: `${R.isEmpty(inputs.yourName) ? 'anonymous' : inputs.yourName}`,
+      email: `${R.isEmpty(inputs.yourEmail) ? 'anonymous' : inputs.yourEmail}`,
+      body: [
+        {
+          _type: 'block',
+          markDefs: [],
+          children: [{ _type: 'span', text: `${inputs.yourStory}`, marks: [] }],
+        },
+      ],
     }
+
+    client
+      .create(story)
+      .then(() => {
+        // trigger confirmation modal here
+        setInputs(initialState)
+        toggleSubmissionModal(true)
+      })
+      .catch(e => console.log('error uploading story', e)) //eslint-disable-line
   }
 
-  return (
-    <div className="flex flex-col mb-5 font-sm">
-      <Label>{title}</Label>
-      <Field />
-    </div>
+  const [formCompleted, updateFormCompleted] = useState(false)
+  const [inputs, handleInputChange, handleSubmit] = useForm(
+    R.mergeAll(
+      R.map(input => R.objOf(toCamelCase(input.title), ''), inputsFromSanity),
+    ),
+    uploadStoryToSanity,
   )
-}
 
-const ShareStory = ({ body, inputs, subtitle }) => {
-  const [formCompleted] = useState(false)
+  useEffect(
+    () =>
+      inputs.yourStory === ''
+        ? updateFormCompleted(false)
+        : updateFormCompleted(true),
+    [inputs.yourStory],
+  )
+
+  // const [confirmationModal, toggleConfirmationModal] = useState(false)
+  const [submissionModal, toggleSubmissionModal] = useState(false)
+
   return (
     <ShareStoryStyled>
       <Title>{subtitle}</Title>
@@ -67,30 +83,37 @@ const ShareStory = ({ body, inputs, subtitle }) => {
         className="font-sm font-normal text-gray"
         imageOptions={{ w: 320, h: 240, fit: 'max' }}
       />
-      <form>
+      <form key="form" onSubmit={handleSubmit}>
         {R.map(
           ({ title, required, type: [type] }) => (
             <FormFieldWithLabel
-              key={title}
+              name={toCamelCase(title)}
+              value={inputs[toCamelCase(title)]}
+              onChange={handleInputChange}
+              key={toCamelCase(title)}
               required={required}
               title={title}
               type={type}
             />
           ),
-
-          inputs,
+          inputsFromSanity,
         )}
-
+        {/* TODO : add confirmation modal and final submission */}
         <PurpleButton
-          className={formCompleted ? 'mb-2.5' : 'mb-2.5 opacity-50'}
+          as="button"
+          className={
+            formCompleted ? 'mb-2.5 w-full' : 'w-full mb-2.5 opacity-50'
+          }
           type="submit"
         >
-          Submit story
+          Share your story
         </PurpleButton>
+        {/* TODO : add delete modal and functionality */}
         <CoralButton className={formCompleted ? '' : 'opacity-50'}>
-          Delete story
+          Delete your story
         </CoralButton>
       </form>
+      {submissionModal && <p>{confirmationText}</p>}
     </ShareStoryStyled>
   )
 }
