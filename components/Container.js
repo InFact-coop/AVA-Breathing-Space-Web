5e6bed9adbd3a2d7b8e914f14dccbbae6dd37e64
lucymk <lucy.m.king92@gmail.com>
2020-06-30 11:05:01 +0100

    adds styles for wider screens

--- components/Container.js
+++ components/Container.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const Container = styled.div.attrs(({ bgColour }) => ({
-  className: `p-5 bg-${bgColour} h-full`,
+  className: `p-5 bg-${bgColour} h-full max-w-256`,
 }))``
 
 export default Container
