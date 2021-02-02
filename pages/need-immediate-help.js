6cc066c3039c8b10c8c87d6d83de1b20ff17e91f
lucymk <lucy.m.king92@gmail.com>
2021-02-02 12:57:27 +0000

    updates button style and relationship questionnaire copy, adds immediate help page

---
+++ pages/need-immediate-help.js
@@ -0,0 +1,12 @@
+import client from '../client'
+import Information from '../components/Information'
+
+const GET_QUESTIONNAIRE = `*[_type == "page" && slug.current == "need-immediate-help"][0]`
+
+const Questionnaire = props => {
+  return <Information background="bg-white" props={props} />
+}
+
+export default Questionnaire
+
+Questionnaire.getInitialProps = () => client.fetch(GET_QUESTIONNAIRE)
