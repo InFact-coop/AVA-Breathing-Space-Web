efd7f5566d7f06582e672e424ef179a4655a3f62
lucymk <lucy.m.king92@gmail.com>
2022-10-19 16:20:21 +0300

    add need immediate help page, link styiling and click to stories from topic page

--- pages/staying-mum/[topic]/index.js
+++ pages/staying-mum/[topic]/index.js
@@ -14,7 +14,7 @@ const GET_TOPIC = `*[_type == "topic" && slug.current == $slug][0]{
   "services": services[]->{ name, "logo": logo.asset->url, "category": categories[0]->{ slug { current }}, "slug": slug.current },
   "articles": *[ _type == "article" && references(^._id) ] { title, "slug": "staying-mum/" + $slug + "/articles/" + slug.current },
   "people": *[ _type == "person" && references(^._id) ] { title, "slug": "staying-mum/" + $slug + "/people/" + slug.current },  
-  "stories": *[ _type == "story" && references(^._id) ] { "title": author, "slug": "stories/" + slug.current }, 
+  "stories": *[ _type == "story" && references(^._id) ] { "title": author, "slug": "staying-mum/stories/all-stories/" + slug.current }, 
 }`
 
 const OuterContainer = styled.div.attrs({
